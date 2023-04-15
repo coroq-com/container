0eae1e22bb89feb04000c0a3f06d69ee043fc046
Ozawa Masayuki <ozawa@coroq.com>
2023-04-15 23:26:29 +0900

    Use coroq/callable-reflector

--- src/SpreadArguments.php
+++ src/SpreadArguments.php
@@ -2,12 +2,8 @@
 declare(strict_types=1);
 namespace Coroq\Container;
 
-use Closure;
-use InvalidArgumentException;
+use Coroq\CallableReflector\CallableReflector;
 use Psr\Container\ContainerInterface;
-use ReflectionFunction;
-use ReflectionFunctionAbstract;
-use ReflectionMethod;
 
 class SpreadArguments {
   const CONTAINER_NAME = '__container';
@@ -21,7 +17,7 @@ class SpreadArguments {
   public function __construct(callable $callable) {
     $this->callable = $callable;
     $this->parameterNames = [];
-    $reflection = $this->reflectionCallable($callable);
+    $reflection = CallableReflector::createFromCallable($callable);
     foreach ($reflection->getParameters() as $parameter) {
       $this->parameterNames[] = $parameter->getName();
     }
@@ -41,31 +37,5 @@ class SpreadArguments {
       }
     }
     return call_user_func_array($this->callable, $arguments);
-  }
-
-  /**
-   * Get a reflection of a callable
-   * @param callable $callable
-   * @return ReflectionFunctionAbstract
-   */
-  private function reflectionCallable(callable $callable): ReflectionFunctionAbstract {
-    if (is_array($callable)) {
-      return new ReflectionMethod($callable[0], $callable[1]);
-    }
-    if ($callable instanceof Closure) {
-      return new ReflectionFunction($callable);
-    }
-    if (is_object($callable)) {
-      return new ReflectionMethod($callable, '__invoke');
-    }
-    if (is_string($callable)) {
-      if (strpos($callable, '::') === false) {
-        return new ReflectionFunction($callable);
-      }
-      return new ReflectionMethod($callable);
-    }
-    // @codeCoverageIgnoreStart
-    throw new InvalidArgumentException('Unknown type of callable. ' . gettype($callable));
-    // @codeCoverageIgnoreEnd
   }
 }
